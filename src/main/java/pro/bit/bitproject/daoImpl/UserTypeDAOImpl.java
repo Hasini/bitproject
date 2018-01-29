67583faff738c5f1a3886e3cb7ebfe4b7fe7e9c6
Hasini Thamarasa <hasinitwg@gmil.com>
2018-01-29 20:55:58 +0530

    DAOImpl package and class

---
+++ src/main/java/pro/bit/bitproject/daoImpl/UserTypeDAOImpl.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package pro.bit.bitproject.daoImpl;
+
+import pro.bit.bitproject.dao.UserTypeDAO;
+
+/**
+ * @author hasini
+ *
+ */
+public class UserTypeDAOImpl implements UserTypeDAO{
+
+}
