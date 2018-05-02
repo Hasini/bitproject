7609eeb4bc0563086bff8b8eb5e8f37cc48946a3
Hasini Thamarasa <hasinitwg@gmil.com>
2018-05-02 19:18:39 +0530

    user and beanch details interface

--- src/main/java/pro/bit/bitproject/dao/BranchDAO.java
+++ src/main/java/pro/bit/bitproject/dao/BranchDAO.java
@@ -1,11 +1,14 @@
 package pro.bit.bitproject.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import pro.bit.bitproject.domain.Branch;
 
-public interface BranchDAO {
-	public Branch createBranch (Branch branch) throws Exception;
-	public Branch updateBranch (String branchdescr);
-	public Branch deleteBranch (String branchdescr);
-	
-
-}
+	public interface BranchDAO {
+		public Branch createBranch (Branch branch) throws Exception;
+		public List<Object> viewBranchDetails() throws SQLException, Exception;
+		public Branch updateBranch (Branch branch) throws SQLException, Exception;
+		public Branch deleteBranch (String code) throws SQLException, Exception;
+		
+	}
--- src/main/java/pro/bit/bitproject/dao/UserDAO.java
+++ src/main/java/pro/bit/bitproject/dao/UserDAO.java
@@ -1,13 +1,18 @@
 package pro.bit.bitproject.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import pro.bit.bitproject.domain.User;
 import pro.bit.bitproject.domain.UserType;
 
-public interface UserDAO {
+public interface UserDAO{
 	
 	
+	public User createUser (User user) throws SQLException, Exception;
+	public User deleteUser(User user) throws SQLException,Exception;
+	public String loginUser (String username,String password,int usertypeid) throws SQLException, Exception;
 	//public UserType getUserTypeCode (String userTypeCode);
-	public List<UserType> getUserTypeCode();
+	public List<UserType> getUserTypeCode(int usertypeid);
 
 }
