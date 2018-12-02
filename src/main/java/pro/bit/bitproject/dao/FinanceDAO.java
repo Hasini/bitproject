94404b15ea5c18093fe93f8c55b007ab174402fc
Hasini Thamarasa <hasinitwg@gmil.com>
2018-12-02 13:17:31 +0530

    Intrface

--- src/main/java/pro/bit/bitproject/dao/FinanceDAO.java
+++ src/main/java/pro/bit/bitproject/dao/FinanceDAO.java
@@ -1,8 +1,17 @@
 package pro.bit.bitproject.dao;
 
-import pro.bit.bitproject.domain.BillInfo;
 
-public interface BillInfoDAO {
-	public BillInfo createBillInfo(BillInfo bi);
+import org.json.JSONArray;
+
+import pro.bit.bitproject.domain.DailyExpense;
+import pro.bit.bitproject.domain.DailyIncome;
+
+public interface FinanceDAO {
+	public void createDailyExpense(DailyExpense dex);
+	void createDailyIncome(DailyIncome dic);
+	JSONArray getUserList ();
+	JSONArray getExpenseTypeList();
+	JSONArray getIncomeTypeList();
+	
 
 }
